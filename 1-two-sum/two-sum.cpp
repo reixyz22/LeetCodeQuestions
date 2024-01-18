81438994dc90b76eb74fda9cb04bbeafbb88f76a
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-01-18 17:51:31 -0600

    Time: 611 ms (5.01%) | Memory: 10.4 MB (97.10%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> output(2,0); //this vector will store the solution
         for(int i =0; i < nums.size();i++){ //loop through nums once
                 for(int j= 0; j < nums.size();j++){ //loop through again to add every valid combo together
-                    if(i==j){break;} //don't add nums[i] to itself as this is an invalid combo
+                    if(i==j){continue;} //don't add nums[i] to itself as this is an invalid combo
                         
                     if (nums[i] + nums[j] == target){ //we found the solution on this case
                         output[0] = i; //slot into output aray
