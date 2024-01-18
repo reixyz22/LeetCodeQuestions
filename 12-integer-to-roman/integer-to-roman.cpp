263873de09b432c264f292a8748e10f23f1181ae
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-01-18 17:58:50 -0600

    Time: 3 ms (89.93%) | Memory: 6.3 MB (74.49%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,25 @@
+class Solution { //roman nums r fucking dumb
+public:
+    string intToRoman(int num) {
+        struct pair{ //letter char struct
+            string letter;
+            int number;
+        };
+        
+        pair dex [] = {{"I",1}, {"IV",4}, {"V",5}, {"IX", 9}, {"X",10}, {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400}, {"D",500}, {"CM",900}, {"M",1000} }; 
+        //array of letter string pair struct
+        string output = ""; //concat this *
+
+        while (true) {
+            for(int i = 12; i >= 0 ;i--){ // per size of codex
+                if(num / dex[i].number >= 1){ // start backwards and check if you can take this out, if you can then do so. ie: XIV(14) - X(10) = IV(4)
+                    output += dex[i].letter; // ^
+                    num -= dex[i].number; // ^
+                    if(num == 0){ return output;}
+                    break;
+                }
+            }
+        }
+        return 0;
+    }
+};
