b8a4055c5815c7d86fd734f9d5578bed6cf86c91
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-06-05 18:27:51 -0500

    Time: 7 ms (54.89%) | Memory: 10 MB (66.84%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -6,9 +6,9 @@ public:
             int number;
         };
         
-        pair dex [] = {{"I",1}, {"IV",4}, {"V",5}, {"IX", 9}, {"X",10}, {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400}, {"D",500}, {"CM",900}, {"M",1000} }; 
-        //array of letter string pair struct
-        string output = ""; //concat this *
+        pair dex [] = {{"I",1}, {"IV",4}, {"V",5}, {"IX", 9}, {"X",10}, {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400}, {"D",500}, {"CM",900}, {"M",1000} };   //array of letter string pair struct
+      
+        string output = ""; //concat this to make your solution*
 
         while (true) {
             for(int i = 12; i >= 0 ;i--){ // per size of codex
