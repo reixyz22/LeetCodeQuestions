0e7ff17d64c982443d46a8c17db42ff543421bb8
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-06-07 23:38:55 -0500

    Time: 6 ms (59.13%) | Memory: 10 MB (49.19%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,25 +1,24 @@
-class Solution {
-public:
-    string intToRoman(int num) {
-        struct pair{ //letter char struct
-            string letter;
-            int number;
-        };
-        
-        pair dex [] = {{"I",1}, {"IV",4}, {"V",5}, {"IX", 9}, {"X",10}, {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400}, {"D",500}, {"CM",900}, {"M",1000} };   //array of letter string pair struct
-      
-        string output = ""; //concat this to make your solution*
+class Solution { public:string intToRoman(int input) {
+    
+    string output = ""; //concat this with Romans to make your solution / output
 
-        while (true) {
-            for(int i = 12; i >= 0 ;i--){ // per size of codex
-                if(num / dex[i].number >= 1){ // start backwards and check if you can take this out, if you can then do so. ie: XIV(14) - X(10) = IV(4)
-                    output += dex[i].letter; // ^
-                    num -= dex[i].number; // ^
-                    if(num == 0){ return output;}
-                    break;
-                }
+    struct pair{ //letter char pair in c++
+        string letter;
+        int number;
+    };
+    pair dex [] = {{"I",1}, {"IV",4}, {"V",5}, {"IX", 9}, {"X",10}, {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400}, {"D",500}, {"CM",900}, {"M",1000} };   //array of letter string pairs
+    
+
+    while (true) { //loop through the input
+        for(int i = 12; i >= 0 ;i--){ // loop through the pairs starting from the back
+            if(input / dex[i].number >= 1){ // start backwards(checking biggest numbers first) and check if you can take this out, if you can then do so. IE: (14)XIV - (10)X = (4)IV
+                output += dex[i].letter; // put the Roman into the output IE X
+                input -= dex[i].number; // take the corresponding number out of the input IE num -=10 
+                if(input == 0){ return output; } //if you 0 out, IE 11 - X = 1. 1 - I == 0 end the program
+                break; //if not move onto next part of the input
             }
         }
-        return 0;
     }
-};
+
+return 0; // the program never hits this as num==0 will always trigger, but we need this to compile in c++
+}};
