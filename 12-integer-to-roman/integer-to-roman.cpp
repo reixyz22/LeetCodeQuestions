be1da7e3803cbe5d0a0d60bb9bd1b57967a119a9
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-06-09 14:03:28 -0500

    Time: 4 ms (74.00%) | Memory: 9.9 MB (67.03%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,23 +1,25 @@
-#include <vector>
-#include <string>
-#include <utility>
+class Solution {public:string intToRoman(int input) {
+    
+    string output;
 
-class Solution {
-public:
-    std::string intToRoman(int num) {
-        std::vector<std::pair<int, std::string>> values = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
+    struct pair {
+        int number;
+        string letter;
+    };   
+    
+    pair dex[] = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
 
-        std::string result;
-        for (auto& [value, roman] : values) {
-            while (num >= value) {
-                result += roman;
-                num -= value;
-            }
+
+    for (int i = 0; i < 13; ++i) {
+        while (input >= dex[i].number) {
+            output += dex[i].letter;
+            input -= dex[i].number;
         }
-        return result;
     }
-};
+    return output;
+
+}};
