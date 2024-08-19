09c36d72948edcf07681cdd1c8b6302d865dd07e
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-08-19 16:32:04 -0500

    Time: 23 ms (9.64%) | Memory: 11.5 MB (92.34%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def reverse(self, x):
-        negative = 1
+        minus = 1
         if x < 0:
             x = x * -1
-            negative = -1
+            minus = -1
         output = int(''.join(reversed(str(x))))
         if output > 2**31:
             return 0
         else:
-            return output * negative
+            return output * minus
         
