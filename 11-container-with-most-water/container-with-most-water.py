e13613e447fd04abae0622350c4a0e069f526005
William Pitts <66898056+reixyz22@users.noreply.github.com>
2024-10-26 16:43:02 -0500

    Time: 91 ms (69.02%) | Memory: 19.9 MB (96.38%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,17 +1,11 @@
 class Solution:
-    @staticmethod
-    def volume_calc(left, right, heights):
-        distance = right - left
-        top = min(heights[left], heights[right])
-        return top * distance
-
     def maxArea(self, heights):
         volume = 0
         left = 0
         right = len(heights) - 1
 
         while left < right:
-            local_volume = Solution.volume_calc(left, right, heights)
+            local_volume = (right - left) * min(heights[left], heights[right]) 
             volume = max(volume, local_volume)
             if heights[left] > heights[right]:
                 right -= 1
