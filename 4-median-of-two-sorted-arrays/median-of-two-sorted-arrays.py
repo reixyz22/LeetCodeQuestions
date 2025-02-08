d18f8bc15bcb52cbb6576b6f56a5dd8b8d9aa9ec
William Pitts <66898056+reixyz22@users.noreply.github.com>
2025-02-08 12:02:11 -0600

    Time: 2 ms (59.56%) | Memory: 17.9 MB (74.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,42 +1,9 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        combined_length = len(nums1) + len(nums2)
-        required_moves = combined_length // 2 + 1
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
-        if combined_length % 2 == 0:
-            one_more = True
+        lst3 = sorted(nums1 + nums2) # just combine them as python does this pretty fast
+
+        if len(lst3) % 2 == 0:
+            return (lst3[len(lst3) // 2] + lst3[len(lst3) // 2 - 1] ) / 2 # middle of an even length array ex [1, 2, 3, 4] -> 2 + 3 // 2 = 5 / 2 = 2.5 
         else:
-            one_more = False
-
-        top_count = 0
-        bottom_count = 0
-
-        middle = -1
-        prev = -1  # To store the previous element for even-length arrays
-
-        while True:
-            # Check for out-of-bounds
-            if top_count >= len(nums1):
-                current = nums2[bottom_count]
-                bottom_count += 1
-            elif bottom_count >= len(nums2):
-                current = nums1[top_count]
-                top_count += 1
-            else:
-                if nums1[top_count] > nums2[bottom_count]:
-                    current = nums2[bottom_count]
-                    bottom_count += 1
-                else:
-                    current = nums1[top_count]
-                    top_count += 1
-
-            if top_count + bottom_count == required_moves:
-                if not one_more:
-                    return current
-                else:
-                    # Combine the two middle elements for even-length arrays
-                    return (prev + current) / 2
-
-            # Update the previous element
-            prev = current
-
+            return lst3[len(lst3) // 2] # middle of odd array
