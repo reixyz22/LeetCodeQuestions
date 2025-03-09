7b51addbcc9711d3830845827cda026c35fe8526
William Pitts <66898056+reixyz22@users.noreply.github.com>
2025-03-08 18:57:18 -0600

    Time: 0 ms (100.00%) | Memory: 18.7 MB (38.38%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,26 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None:
-            return head
-
-        my_list = [] # store nodes in a list
-
-        while head:
-            my_list.append(head) # add nodes to the list
-            head = head.next
+    def reverseList(self, node: Optional[ListNode]) -> Optional[ListNode]:
+        previous = None  # Previous node starts as None since the new head's next should be None
         
-        # print(my_list) # should be a list of nodes in order with one dummy node at the start
+        while node:
+            next_node = node.next
+            node.next = previous
+            previous = node
+            node = next_node
 
-        for i, node in enumerate(my_list):
-            if i == 0:
-                node.next = None
-            else:
-                node.next = my_list[i-1]
-        
-        # print( my_list[len(my_list) -1 ]  )
-        return my_list[len(my_list) -1]
+        return previous  # Previous will be the new head of the reversed list
