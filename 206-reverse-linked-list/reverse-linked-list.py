29a4ddbf82302a33ced63066db8c05fc525d3db7
William Pitts <66898056+reixyz22@users.noreply.github.com>
2025-03-08 18:43:02 -0600

    Time: 0 ms (100.00%) | Memory: 18.6 MB (53.95%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,21 +1,26 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize the previous pointer to None, as the new end of the list will have no next node
-        previous = None
-        current = head
-        # Iterate over the linked list until all nodes are processed
-        while current:
-            # Temporarily store the next node
-            next_node = current.next
-            # Reverse the current node's pointer
-            current.next = previous
-            # Move pointers one position forward
-            previous = current
-            current = next_node
-        # At the end of the loop, 'previous' will point to the new head of the reversed list
-        return previous
+        if head == None:
+            return head
+
+        my_list = [] # store nodes in a list
+
+        while head:
+            my_list.append(head) # add nodes to the list
+            head = head.next
+        
+        # print(my_list) # should be a list of nodes in order with one dummy node at the start
+
+        for i, node in enumerate(my_list):
+            if i == 0:
+                node.next = None
+            else:
+                node.next = my_list[i-1]
+        
+        # print( my_list[len(my_list) -1 ]  )
+        return my_list[len(my_list) -1]
